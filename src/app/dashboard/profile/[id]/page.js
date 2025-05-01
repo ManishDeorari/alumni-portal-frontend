f4cb8f7bdf637582b716afa7d22387e7914f823f
ManishDeorari <manishdeorari377@example.com>
2025-05-01 08:45:50 +0530

    Fixed unescaped quote issue for production

--- src/app/dashboard/profile/[id]/page.js
+++ src/app/dashboard/profile/[id]/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "../../../components/Sidebar";
 
 export default function OtherProfilePage() {
   const { id } = useParams();
