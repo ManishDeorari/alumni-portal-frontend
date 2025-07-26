3dd6c76c18f695b7c4b8492329bd9dc2b1aecfcd
ManishDeorari <manishdeorari377@example.com>
2025-07-26 17:02:11 +0530

    Fixed unescaped quote issue for production

--- src/app/components/Post/Visual/CommentInput.js
+++ src/app/components/Post/Visual/CommentInput.js
@@ -58,13 +58,12 @@ export default function CommentInput({
           ref={inputRef}
         />
 
-        <button
-          type="button"
-          onClick={() => setShowCommentEmoji(!showCommentEmoji)}
-          className="text-gray-500 hover:text-gray-700"
-        >
-          <FaSmile size={18} />
-        </button>
+        {/* ✅ Emoji Picker */}
+          {showCommentEmoji && (
+            <div ref={emojiRef} className="absolute right-0 z-50 mt-2">
+              <EmojiPicker onEmojiClick={(e, emojiObject) => handleEmojiClick(emojiObject)} />
+            </div>
+          )}
 
         {/* ✅ Comment Submit */}
         <button
@@ -74,13 +73,6 @@ export default function CommentInput({
           Post
         </button>
       </div>
-
-      {/* ✅ Emoji Picker */}
-      {showCommentEmoji && (
-        <div ref={emojiRef} className="absolute right-0 z-50 mt-2">
-          <EmojiPicker onEmojiClick={(e, emojiObject) => handleEmojiClick(emojiObject)} />
-        </div>
-      )}
 
       {/* Typing Indicator */}
       {typing && (
