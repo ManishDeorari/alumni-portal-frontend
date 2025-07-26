e2cc6db66107c18dbdcfac9277f6d1e67e11de05
ManishDeorari <manishdeorari377@example.com>
2025-07-26 21:36:55 +0530

    Fixed unescaped quote issue for production

--- src/app/components/Post/Visual/commentCard.js
+++ src/app/components/Post/Visual/commentCard.js
@@ -30,7 +30,7 @@ export default function CommentCard({ comment, currentUser, onReply, onDelete, o
                 😊
               </button>
               {showEmoji && (
-                <div className="absolute z-10 top-10 right-30">
+                <div className="absolute z-10 top-30 right-60">
                   <Picker
                     onEmojiSelect={(emoji) =>
                       setEditText((prev) => prev + emoji.native)
